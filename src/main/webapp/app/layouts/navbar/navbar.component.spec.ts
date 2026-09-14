c66ed7f16e3385285584aaa4f66856ad4735aff6
RaulBaron373 <raul_bg1998@hotmail.com>
2026-09-14 13:27:44 +0200

    refactor: remove obsolete navbar logic

--- src/main/webapp/app/layouts/navbar/navbar.component.spec.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.spec.ts
@@ -1,37 +1,15 @@
-jest.mock('app/login/login.service');
-
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { of } from 'rxjs';
 import { TranslateModule } from '@ngx-translate/core';
-
-import { Account } from 'app/core/auth/account.model';
-import { AccountService } from 'app/core/auth/account.service';
-import { LoginService } from 'app/login/login.service';
-import { EntityNavbarItems } from 'app/entities/entity-navbar-items';
 
 import NavbarComponent from './navbar.component';
 
 describe('Navbar Component', () => {
   let comp: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
-  let accountService: AccountService;
-  const account: Account = {
-    activated: true,
-    authorities: [],
-    email: '',
-    firstName: 'John',
-    langKey: '',
-    lastName: 'Doe',
-    login: 'john.doe',
-    imageUrl: '',
-  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [NavbarComponent, TranslateModule.forRoot()],
-      providers: [provideHttpClient(), provideHttpClientTesting(), LoginService],
     })
       .overrideTemplate(NavbarComponent, '')
       .compileComponents();
@@ -40,51 +18,27 @@ describe('Navbar Component', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
     comp = fixture.componentInstance;
-    accountService = TestBed.inject(AccountService);
   });
 
-  it('should initialize entity navbar items on init', () => {
-    // WHEN
-    comp.ngOnInit();
-
-    // THEN
-    expect(comp.entitiesNavbarItems).toEqual(EntityNavbarItems);
+  it('should start collapsed', () => {
+    expect(comp.isNavbarCollapsed()).toBe(true);
   });
 
-  it('should hold current authenticated user in variable account', () => {
-    // WHEN
-    comp.ngOnInit();
+  it('should toggle navbar', () => {
+    comp.toggleNavbar();
 
-    // THEN
-    expect(comp.account()).toBeNull();
+    expect(comp.isNavbarCollapsed()).toBe(false);
 
-    // WHEN
-    accountService.authenticate(account);
+    comp.toggleNavbar();
 
-    // THEN
-    expect(comp.account()).toEqual(account);
-
-    // WHEN
-    accountService.authenticate(null);
-
-    // THEN
-    expect(comp.account()).toBeNull();
+    expect(comp.isNavbarCollapsed()).toBe(true);
   });
 
-  it('should hold current authenticated user in variable account if user is authenticated before page load', () => {
-    // GIVEN
-    accountService.authenticate(account);
+  it('should collapse navbar', () => {
+    comp.isNavbarCollapsed.set(false);
 
-    // WHEN
-    comp.ngOnInit();
+    comp.collapseNavbar();
 
-    // THEN
-    expect(comp.account()).toEqual(account);
-
-    // WHEN
-    accountService.authenticate(null);
-
-    // THEN
-    expect(comp.account()).toBeNull();
+    expect(comp.isNavbarCollapsed()).toBe(true);
   });
 });
--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,12 +1,7 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import SharedModule from 'app/shared/shared.module';
-import { AccountService } from 'app/core/auth/account.service';
-import { LoginService } from 'app/login/login.service';
-import { EntityNavbarItems } from 'app/entities/entity-navbar-items';
-import { environment } from 'environments/environment';
-import NavbarItem from './navbar-item.model';
 
 @Component({
   selector: 'jhi-navbar',
@@ -14,38 +9,11 @@ import NavbarItem from './navbar-item.model';
   styleUrl: './navbar.component.scss',
   imports: [RouterModule, SharedModule],
 })
-export default class NavbarComponent implements OnInit {
+export default class NavbarComponent {
   isNavbarCollapsed = signal(true);
-  version = '';
-  account = inject(AccountService).trackCurrentAccount();
-  entitiesNavbarItems: NavbarItem[] = [];
-
-  private readonly loginService = inject(LoginService);
-  private readonly router = inject(Router);
-
-  constructor() {
-    const { VERSION } = environment;
-    if (VERSION) {
-      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
-    }
-  }
-
-  ngOnInit(): void {
-    this.entitiesNavbarItems = EntityNavbarItems;
-  }
 
   collapseNavbar(): void {
     this.isNavbarCollapsed.set(true);
-  }
-
-  login(): void {
-    this.router.navigate(['/login']);
-  }
-
-  logout(): void {
-    this.collapseNavbar();
-    this.loginService.logout();
-    this.router.navigate(['']);
   }
 
   toggleNavbar(): void {
