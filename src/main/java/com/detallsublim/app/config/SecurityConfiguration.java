8c0281ede242abd83f93b85ce5fa3e61ca65a53c
RaulBaron373 <raul_bg1998@hotmail.com>
2026-09-20 20:15:09 +0200

    fix(security): disable public password reset init

--- src/main/java/com/detallsublim/app/config/SecurityConfiguration.java
+++ src/main/java/com/detallsublim/app/config/SecurityConfiguration.java
@@ -87,7 +87,7 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern(HttpMethod.GET, "/api/authenticate")).permitAll()
                     .requestMatchers(mvc.pattern("/api/register")).denyAll()
                     .requestMatchers(mvc.pattern("/api/activate")).denyAll()
-                    .requestMatchers(mvc.pattern("/api/account/reset-password/init")).permitAll()
+                    .requestMatchers(mvc.pattern("/api/account/reset-password/init")).denyAll()
                     .requestMatchers(mvc.pattern("/api/account/reset-password/finish")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
 
